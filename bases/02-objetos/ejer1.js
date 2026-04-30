275d967daea633a58c81bdaf036a38442b120732
diegokld30 <dfercalderon@poligran.edu.co>
2026-04-30 16:01:25 -0500

    Optimizando el ejercicio 1

--- bases/02-objetos/ejer1.js
+++ bases/02-objetos/ejer1.js
@@ -1,1 +1,18 @@
-let nombre = "Diego";
+function suma(numero1, numero2) {
+    let resultado = numero1 + numero2;
+    return resultado;
+}
+function resta(numero1, numero2) {
+    let resultado = numero1 - numero2;
+    return resultado;
+}
+function multi(numero1, numero2) {
+    let resultado = numero1 * numero2;
+    return resultado;
+}
+function divi(numero1, numero2) {
+    let resultado = numero1 / numero2;
+    return resultado;
+}
+
+console.log(suma(13, divi(34,4)));
